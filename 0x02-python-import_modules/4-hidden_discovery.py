dd4f8437a6a03f7388418068c74e14402014224c
chisomchinaza <chisomchinazam@gmail.com>
2024-03-14 21:52:42 -0700

    A program that prints all the names defined by the compiled module hidden_4.pyc

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from hidden_4 import *
-    allf = dir()
-    for i in range(0, len(allf)):
-        if allf[i][:2] != "__":
-            print("(:s)".format (allf(i)))
+    import hidden_4
+    for s in dir(hidden_4):
+        if s[:2] != "__":
+            print("{:s}".format(s))
